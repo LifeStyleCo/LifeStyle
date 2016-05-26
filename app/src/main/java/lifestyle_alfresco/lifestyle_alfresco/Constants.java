c82f88188fa90febbf1f235926f9620403a33718
Nan <boris2800@gmail.com>
2016-05-26 03:59:40 +0300

    112

--- app/src/main/java/lifestyle_alfresco/lifestyle_alfresco/Constants.java
+++ app/src/main/java/lifestyle_alfresco/lifestyle_alfresco/Constants.java
@@ -6,9 +6,4 @@ public class Constants {
     public static final int TAB_TWO = 1;
     public static final int TAB_THREE = 2;
 
-    //public static class URL {
-    //    private static final String HOST = "http://192.168.0.101:8080/";
-    //    public static final String GET_REMIND = HOST + "reminder/get";
-    //}
-
 }
